4b43074ae2e6e2fac70c03cb8652218807dd031d
Jonas Bloch <jonasbloch@pop-os.localdomain>
2023-07-25 16:28:22 +0200

    Updated using generators

--- src/component.rs
+++ src/component.rs
@@ -12,7 +12,8 @@
 |
 -----------------------------------------------------------------------*/
 
-use crate::dipole::Dipole;
+use crate::{dipole::Dipole, generator::Generator as Gen};
+use Dipole::{F, R, L, C};
 
 /* ------------------------------------------------------------------- */
 
@@ -22,6 +23,7 @@ pub(crate) enum ComponentContent<T: num::Float + std::fmt::Debug> {
     Serial(Vec<Component<T>>),
     Parallel(Vec<Component<T>>),
 }
+use ComponentContent::Simple;
 
 #[derive(Debug, Clone)]
 pub(crate) struct Component<T: num::Float + std::fmt::Debug> {
@@ -29,16 +31,80 @@ pub(crate) struct Component<T: num::Float + std::fmt::Debug> {
     pub(crate) current: T,
     pub(crate) equiv: Dipole<T>,
     /*
-    Explanation : at t = 0+
-    ->  R means the component is equivalent to a resistor
-    ->  C means the tension is necessarily 0 (equivalence with a capacitor),
+    Explanation :
+    ->  R means the component is equivalent to a Norton/Thevenin generator
+    ->  C means the tension is imposed by the dipole (equivalence with a capacitor),
         which happens if the component is a capacitor or if it is a parallel
         component with a capacitor in its branches, or a serial component with only capacitors, etc...
-    ->  L means the current is necessarily 0 for reasons similar to the former case's
+    ->  L means the current is imposed by the dipole for reasons similar to the former case's
         (equivalence with a coil)
 
     ->  This whole is necessary to determine tension of sub-components in a serial component
         and current of sub-components in a parallel component
     */
     pub(crate) content: ComponentContent<T>,
+}
+
+impl<T: num::Float + num::Zero + std::fmt::Debug> Default for Component<T> {
+    fn default() -> Self {
+        let zero = T::zero();
+        Self {
+            tension: zero,
+            current: zero,
+            equiv: F,
+            content: Simple(F),
+        }
+    }
+}
+
+impl<T: num::Float + num::Zero + std::fmt::Debug> Component<T> {
+    pub(crate) fn new_r(_r: T) -> Self {
+        let zero = T::zero();
+        if _r <= zero {
+            panic!("Tried to build a negative or zero resistance")
+        } else {
+            Self {
+                tension: zero,
+                current: zero,
+                equiv: R(Gen {
+                    r: _r,
+                    source: zero,
+                    t_or_n: true,
+                }),
+                content: Simple(R(Gen {
+                    r: _r,
+                    source: zero,
+                    t_or_n: true,
+                })),
+            }
+        }
+    }
+    pub(crate) fn new_c(c: T, u: T) -> Self {
+        // u is the starting charge of the capacitor
+        let zero = T::zero();
+        if c <= zero {
+            panic!("Tried to build a negative or zero capacitor")
+        } else {
+            Self {
+                tension: u,
+                current: zero,
+                equiv: C(c),
+                content: Simple(C(c)),
+            }
+        }
+    }
+    pub(crate) fn new_l(l: T) -> Self {
+        let zero = T::zero();
+        if l <= zero {
+            panic!("Tried to build a negative or zero inductance")
+        } else {
+            Self {
+                tension: zero,
+                current: zero,
+                // We don't suppose the coil is charged initially
+                equiv: L(l),
+                content: Simple(L(l)),
+            }
+        }
+    }
 }
