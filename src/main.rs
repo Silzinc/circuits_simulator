e0d1a5b2d6dee7d5d397f4e59e09099bf3f0ea2c
Jonas Bloch <jonasbloch@pop-os.localdomain>
2023-07-25 16:29:17 +0200

    Changed test

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@
 |
 -----------------------------------------------------------------------*/
 
+mod generator;
 mod dipole;
 mod component;
 mod initial;
@@ -19,42 +20,42 @@ pub(crate) fn type_of<T>(_: &T) -> String {
     String::from(std::any::type_name::<T>())
 }
 
+use std::io::Write;
+
 /* ------------------------------------------------------------------- */
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     use component::Component as Com;
-
+    
     let mut comp = Com::<f32>::default();
     let mut rc = Com::<f32>::default();
     let mut rlc = Com::<f32>::default();
 
-    rc.push_serie(Com::<f32>::new_r(500.));
-    rc.push_serie(Com::<f32>::new_c(0.002, 0.));
+    rc.push_serie(Com::<f32>::new_r(1.));
+    rc.push_serie(Com::<f32>::new_c(1., 0.));
 
-    rlc.push_serie(Com::<f32>::new_r(500.));
-    rlc.push_serie(Com::<f32>::new_l(0.001));
-    rlc.push_serie(Com::<f32>::new_c(0.002, 0.)); 
+    // rlc.push_serie(Com::<f32>::new_r(1.));
+    rlc.push_serie(Com::<f32>::new_l(1.));
+    rlc.push_serie(Com::<f32>::new_c(1., 0.)); 
 
-    comp.push_parallel(rc);
-    // comp.push_parallel(rlc);    
+    // comp.push_parallel(rc);
+    comp.push_parallel(rlc);    
     
     let mut circuit = circuit::Circuit {
         dt: 0.01,
-        source: 100.,
+        source: 10.,
         charge: comp,
     };
 
     let mut values = Vec::<f32>::new();
     circuit.setup();
     
-    for _ in 0..500 {
-        values.push(match &circuit.charge.content {
-            crate::component::ComponentContent::Serial(comps) => comps[1].current,
-            _ => unreachable!(),
-        });
+    for _ in 0..=10000 {
+        values.push(circuit.charge.current);
         circuit.update();
     }
 
-    println!("{:?}", values);
+    let mut txt = std::fs::File::create("values.txt")?;
+    txt.write_all(format!("{:?}", values).as_bytes())?;
     Ok(())
 }
