2d33ed3349969818bb95b65fec10c8914df26c31
Silzinc <jonasbloch85@gmail.com>
2023-10-06 22:14:39 +0200

    Gave better shape to tests.

--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,7 @@
 #[allow(dead_code)]
-pub(crate) fn test() -> crate::error::Result<()>
+fn test_() -> crate::error::Result<()>
 {
+	use crate::fourier::fouriers;
 	use crate::structs::{
 		circuit::Circuit,
 		component::Component,
@@ -18,31 +19,29 @@ pub(crate) fn test() -> crate::error::Result<()>
 			1.
 		}
 	}
-	fn triangle(x: f64) -> f64 { ((x + 2.) % 4.) - 2. }
+	fn triangle(x: f64) -> f64 { (x % 2. - 1.).abs() }
 	fn weird(x: f64) -> f64 { (x.abs() + 1.).ln() + x.abs().sqrt().sin() - (x / 10.).exp() }
 	fn sine(x: f64) -> f64 { (2. * x).sin() + (4. * x).sin() / 2. + (6. * x).sin() / 3. }
 
 	let fundamental = 0.04;
 	let n_freqs = 5000;
-	let fourier_coefs = crate::fourier::fouriers(sine, fundamental, n_freqs - 1);
+	let fourier_coefs = fouriers(door, fundamental, n_freqs - 1);
 
 	// Create the circuit
 	let mut c = Circuit::new();
-	c.source.add_pulse(0., fourier_coefs[0]);
-	for (i, coef) in fourier_coefs.iter().enumerate().skip(1) {
+	for (i, coef) in fourier_coefs.iter().enumerate() {
 		c.source.add_pulse(twopi * i as f64 * fundamental, *coef);
 		// This line would be required if we did not use the fourier real functions
-		// approximation in circuit.rs line 436
+		// approximation in emulation.rs line 41
 		// c.source.add_pulse(-twopi * i as f64 * fundamental, coef.conj());
 	}
-
 	c.content.push_serie(Component::try_from(Resistor(1.))?);
 	c.content.push_serie(Component::try_from(Capacitor(0.25))?);
 
 	// Simulate the circuit
 	let duration = 1. / (2. * fundamental) * 0.99; // Gotta respect the Shannon-Nyquist criterion
 	let step = 0.02;
-	let (currents, tensions) = c.emulate_one(duration, step, &vec![0u8])?;
+	let (currents, tensions, _) = c.emulate_one(duration, step, &vec![0u8])?;
 
 	// Save the results to a file
 	let out_dir = std::path::Path::new(r"/home/jonasbloch/Projets/circuits_rust/out");
@@ -61,4 +60,12 @@ pub(crate) fn test() -> crate::error::Result<()>
 	                                     .spawn()
 	                                     .expect("Failed to plot the results");
 	Ok(())
+}
+
+#[test]
+fn test()
+{
+	if let Err(e) = test_() {
+		eprintln!("{}", e);
+	}
 }
