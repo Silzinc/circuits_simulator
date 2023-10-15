9ff853e6b2b1af7502cad4fba1b2c0c567318097
Silzinc <jonasbloch85@gmail.com>
2023-10-15 14:51:30 +0200

    Changed test to display a plot of a filtered square wave in a RC circuit.

--- out/plot.py
+++ out/plot.py
@@ -6,27 +6,27 @@ from sys import argv
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # Load data
-with open("currents.txt", 'r') as currents_file:
-  currents = [float(s) for s in currents_file.read()[1:-1].split(', ')]
-with open("tensions.txt", 'r') as tensions_file:
-  tensions = [float(s) for s in tensions_file.read()[1:-1].split(', ')]
+with open("tensions1.txt", 'r') as tensions1_file:
+  tensions1 = [float(s) for s in tensions1_file.read()[1:-1].split(', ')]
+with open("tensions2.txt", 'r') as tensions2_file:
+  tensions2 = [float(s) for s in tensions2_file.read()[1:-1].split(', ')]
 
-assert len(currents) == len(tensions)
+assert len(tensions1) == len(tensions2)
 
 # Parameters
-duration = float(argv[1])
-t = np.linspace(0, duration, len(currents))
+duration = float(argv[1]) # Duration of the simulation passed by Rust
+t = np.linspace(0, duration, len(tensions1))
 
 # Plot
 fig = plt.figure()
-fig.suptitle("Currents and tensions over time")
+fig.suptitle("Tensions over time in a RC circuit")
 ax1, ax2 = fig.subplots(2, 1)
 
-ax1.plot(t, currents)
-ax1.set_ylabel("Current (A)")
+ax1.plot(t, tensions1)
+ax1.set_ylabel("Tension on the source (V)")
 
-ax2.plot(t, tensions)
-ax2.set_ylabel("Tension (V)")
+ax2.plot(t, tensions2)
+ax2.set_ylabel("Tension on the capacitor (V)")
 
 plt.xlabel("Time (s)")
 plt.show()
