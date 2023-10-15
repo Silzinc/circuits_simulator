8d1abda162b147ccd64d6d5d6f7ad4bdd42f3595
Silzinc <jonasbloch85@gmail.com>
2023-10-15 15:28:35 +0200

    Generated crate-level documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! A crate that can be used to simulate simple electrical circuits. It
+//! currently supports analyzing circuits with resistors, capacitors, inductors,
+//! and "equivalent impedance" components represented by their impedance. Said
+//! impedance is represented by a rational fraction of the pulse with complex
+//! coefficients.
+//!
+//! A circuit can only be made of one source and a tree of components. Said
+//! components are either made of parallel or serial combinations of other
+//! components.
+
 mod emulation;
 pub mod error;
 pub mod fourier;
