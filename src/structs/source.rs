0e328ea587468ea991b4d42969f91fdbecf9d36f
Silzinc <jonasbloch85@gmail.com>
2024-01-03 13:05:35 +0100

    Added a `set_fn` function to clear and reconstruct the source from a function.

--- src/structs/source.rs
+++ src/structs/source.rs
@@ -45,32 +45,49 @@ impl Source
 	}
 
 	/// Removes the pulse at the specified index from the `voltages` vector.
+	#[inline]
 	pub fn remove_pulse(&mut self, index: usize) { self.voltages.remove(index); }
-}
 
-impl Source
-{
+	/// Clears the `voltages` vector.
+	#[inline]
+	pub fn clear(&mut self) { self.voltages.clear(); }
+
 	/// Creates a new `Source` from a real valued function that generates voltage
 	/// values using its Fourier transform. The function takes a time value as
 	/// input and returns a voltage value. The `duration` parameter specifies the
 	/// total duration of the voltage source (henceforth the duration of the
 	/// simulation). The `n_freqs_` parameter specifies the number of frequencies
 	/// to use in the Fourier series.
+	#[inline]
 	pub fn from_fn<I, F>(f: F, duration: f64, n_freqs_: I) -> Self
 		where F: Fn(f64) -> f64,
 		      I: PrimInt + Debug
 	{
+		let mut source = Self::new();
+		source.set_fn(f, duration, n_freqs_);
+		source
+	}
+
+	/// Clears and updates `self` using a real valued function that generates
+	/// voltage values using its Fourier transform. The function takes a time
+	/// value as input and returns a voltage value. The `duration` parameter
+	/// specifies the total duration of the voltage source (henceforth the
+	/// duration of the simulation). The `n_freqs_` parameter specifies the number
+	/// of frequencies to use in the Fourier series.
+	pub fn set_fn<I, F>(&mut self, f: F, duration: f64, n_freqs_: I)
+		where F: Fn(f64) -> f64,
+		      I: PrimInt + Debug
+	{
+		self.clear();
 		let fundamental = (duration + duration).recip(); // Shannon's theorem
 		let n_freqs = n_freqs_.to_usize().expect(&format!("Failed to convert {n_freqs_:?} to usize"));
-		let mut source = Self { voltages: Vec::with_capacity(n_freqs), };
 		let fourier_coefs = fouriers(f, fundamental, n_freqs - 1);
 		let twopif = fundamental * 2. * std::f64::consts::PI;
 
 		let mut pulse = 0f64;
 		for coef in fourier_coefs {
-			source.add_pulse(pulse, coef);
+			self.add_pulse(pulse, coef);
 			pulse += twopif;
 		}
-		source
 	}
 }
