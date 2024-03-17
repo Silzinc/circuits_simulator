5530ca0400d3fd709d04021a4aebe0c6c6168f69
Silzinc <jonasbloch85@gmail.com>
2024-03-17 12:21:31 +0100

    Defined id macro

--- src/structs/node.rs
+++ src/structs/node.rs
@@ -1,10 +1,16 @@
 use num::Complex;
 
-// IMPROVEMENT: A linked list would probably be a better fit
 /// A node is a point in a circuit where two or more circuit components meet. It
 /// is represented by a vector of bytes that gives the path to find it in the
 /// node tree.
 pub type Id = Vec<u8>;
+
+/// A macro to create an ID. This is just a copy of the `vec!` macro.
+#[macro_export]
+macro_rules! id {
+  ($($x:expr),*) => (vec![$($x),*]);
+  ($($x:expr);*) => (vec![$($x);*]);
+}
 
 /// A node in a circuit.
 #[derive(Clone, Debug, Default)]
